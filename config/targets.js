e8e96884e1efa7548a797858b74fa3f8c0e34c9a
Thomas Wang <wangxgwxg@gmail.com>
2021-02-20 21:25:02 -0800

    Drop ie support

--- config/targets.js
+++ config/targets.js
@@ -1,13 +1,6 @@
 'use strict';
 
-const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
-
-const isCI = Boolean(process.env.CI);
-const isProduction = process.env.EMBER_ENV === 'production';
-
-if (isCI || isProduction) {
-  browsers.push('ie 11');
-}
+const browsers = ['last 2 Chrome versions', 'last 2 Firefox versions', 'last 2 Safari versions'];
 
 module.exports = {
   browsers,
